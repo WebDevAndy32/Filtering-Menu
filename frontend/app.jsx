3c7d538964155f0c90592e667732cafca8eee1bf
WebDevAndy32 <33171972+WebDevAndy32@users.noreply.github.com>
2022-02-20 07:11:21 -0600

    Update app.jsx
    added selection buttons

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -2,23 +2,33 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      database: ["blank"]
+      database: ["blank"],
+      selections: {name: 'blank',
+                   brand: 'blank', 
+                   color: 'blank', 
+                   caffeine: ['high', 'medium', 'low', 'none'], 
+                   type: ['black', 'green', 'white', 'herbal'], 
+                   tastesLike: ['sweet', 'citrusy', 'smokey', 'bitter', 'aromatic', 'funky', 'spicy', 'floral', 'smooth', 'fruity', 'neutral'], 
+                   ingredients: 'blank'
+                  }
     }
     this.selectionMenu = this.selectionMenu.bind(this);
     this.filteredMatches = this.filteredMatches.bind(this);
     this.getDatabase = this.getDatabase.bind(this);
+    this.buildSelections = this.buildSelections.bind(this);
   }
   
   selectionMenu = () => {
     
-    
-    return(
-     "I am selectionMenu"
-    );
+    return this.buildSelections();
   }
-        
+  //onclick for selection buttons should update filteredMatches, but how      
   filteredMatches = () => {
+    //selected buttons define search criteria
     
+    //use search criteria to match tea objects
+    
+    //display matching teas on screen
     return (
      "I am filteredMatched"
     );
@@ -38,9 +48,24 @@ class App extends React.Component{
       console.error('Error:', error);
     });
   }
+  
+  buildSelections = () => {
+   let selectionKeys = Object.keys(this.state.selections);
+   let elementCollector = [];
+   selectionKeys.forEach(ki => {
+     let newElement = (
+     <div id={'sel-' + ki} key={'sel-' + ki} className='selections-button'>
+         {ki}
+     </div>    
+     );
+     elementCollector.push(newElement);
+   });
+    return elementCollector;
+  }
+  
   componentDidMount(){
     this.getDatabase();
-
+    this.buildSelections();
   }  
 
   render(){
