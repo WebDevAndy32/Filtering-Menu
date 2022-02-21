96431068ea5b6e3861cd64e5eca55c74804db790
WebDevAndy32 <33171972+WebDevAndy32@users.noreply.github.com>
2022-02-21 00:02:58 -0600

    Update app.jsx
    expanding menu and buttons, two new functions to handle event clicks in menu

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -3,19 +3,21 @@ class App extends React.Component{
     super(props);
     this.state = {
       database: ["blank"],
-      selections: {name: 'blank',
-                   brand: 'blank', 
-                   color: 'blank', 
+      selections: {name: ['blank'],
+                   brand: ['blank'], 
+                   color: ['blank'], 
                    caffeine: ['high', 'medium', 'low', 'none'], 
                    type: ['black', 'green', 'white', 'herbal'], 
                    tastesLike: ['sweet', 'citrusy', 'smokey', 'bitter', 'aromatic', 'funky', 'spicy', 'floral', 'smooth', 'fruity', 'neutral'], 
-                   ingredients: 'blank'
+                   ingredients: ['blank']
                   }
     }
     this.selectionMenu = this.selectionMenu.bind(this);
     this.filteredMatches = this.filteredMatches.bind(this);
     this.getDatabase = this.getDatabase.bind(this);
     this.buildSelections = this.buildSelections.bind(this);
+    this.selectionsClick = this.selectionsClick.bind(this);
+    this.optionsClick = this.optionsClick.bind(this);
   }
   
   selectionMenu = () => {
@@ -53,14 +55,49 @@ class App extends React.Component{
    let selectionKeys = Object.keys(this.state.selections);
    let elementCollector = [];
    selectionKeys.forEach(ki => {
+     let optionButtons = [];
+
+     this.state.selections[ki].forEach(option => {
+       let newButton = (
+          <div id={'opt-' + ki + '-' + option} key={'opt-' + ki + '-' + option} onClick={this.optionsClick} className='selections-button' >
+           {option}
+          </div>
+       );
+        optionButtons.push(newButton);  
+     });
+     
      let newElement = (
-     <div id={'sel-' + ki} key={'sel-' + ki} className='selections-button'>
+     <div id={'sel-' + ki} key={'sel-' + ki} onClick={this.selectionsClick} className='selections-button'>
          {ki}
+         <div id={'sel-options-' + ki} id={'sel-options-' + ki} className='options-space'>
+           {optionButtons}
+         </div>
      </div>    
      );
      elementCollector.push(newElement);
    });
     return elementCollector;
+  }
+  
+  selectionsClick = () => {
+    let buttonId = event.target.id;
+    let clickedButton = document.getElementById(buttonId);
+    
+    clickedButton.classList.toggle('sel-but-active');    
+    
+    let stateId = buttonId.slice(4);
+    let selOptSpace = document.getElementById('sel-options-' + stateId);
+    let stateValues = this.state.stateId;
+    
+    selOptSpace.classList.toggle('opt-space-active');
+    
+  }
+  
+  optionsClick = () => {
+    let buttonId = event.target.id;
+    let clickedButton = document.getElementById(buttonId);
+    
+    clickedButton.classList.toggle('sel-but-active'); 
   }
   
   componentDidMount(){
